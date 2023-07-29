7f227c72d4699502eee1ae31bcc00ed95794ef18
araschang0827 <b09704076@ntu.edu.tw>
2023-07-29 17:44:47 +0800

    activate volumebomb

--- Routes/Api.py
+++ Routes/Api.py
@@ -1,8 +1,9 @@
 from flask_restful import Api
 from flask import Flask
 import Application.MagicFormula.scheduler
+import Application.VolumeBomb.scheduler
 
 app = Flask(__name__)
 api = Api(app)
 
-# I love aras and I love you
+# I love aras and I love you <- by my girlfriend :D
