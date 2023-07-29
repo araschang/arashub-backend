68a7d904a4475b8f321b05bba654a783b50259e4
araschang0827 <b09704076@ntu.edu.tw>
2023-07-30 00:49:32 +0800

    add stability checker

--- Route/Api.py
+++ Route/Api.py
@@ -1,9 +1,22 @@
 from flask_restful import Api
 from flask import Flask
+from apscheduler.schedulers.background import BackgroundScheduler
+from Base.Connector import DiscordConnector
+from Base.ConfigReader import Config
+from datetime import datetime, timedelta
 import Application.MagicFormula.scheduler
 import Application.VolumeBomb.scheduler
 
 app = Flask(__name__)
 api = Api(app)
+
+def stable_check():
+    discord = DiscordConnector()
+    config = Config()
+    message = 'I am alive.'
+    discord.sendMessage(config['Discord']['DEV'], message)
+sched = BackgroundScheduler()
+sched.add_job(stable_check, 'interval', hours=12, next_run_time=datetime.now() + timedelta(seconds=10))
+sched.start()
 
 # I love aras and I love you <- by my girlfriend :D
