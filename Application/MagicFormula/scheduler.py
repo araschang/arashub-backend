8dcde156d243ec73b01b0b03aa4af7a59f43efe4
araschang0827 <b09704076@ntu.edu.tw>
2023-08-04 14:47:47 +0800

    🐛 fix scheduler miss bugs

--- Application/MagicFormula/scheduler.py
+++ Application/MagicFormula/scheduler.py
@@ -4,11 +4,10 @@ from datetime import datetime, timedelta
 import asyncio
 
 def run_magic_formula():
-    print('fuck')
     asyncio.run(MagicFormula().run())
 
 
 scheduler = BackgroundScheduler(job_defaults={'max_instances': 3})
-scheduler.add_job(run_magic_formula, 'cron', day_of_week='mon-fri', hour=13, minute=30, next_run_time=datetime.now() + timedelta(seconds=10))
+scheduler.add_job(run_magic_formula, 'cron', day_of_week='mon-fri', hour=13, minute=30)
 # scheduler.add_job(run_magic_formula, 'interval', seconds=60, next_run_time=datetime.now() + timedelta(seconds=10))
 scheduler.start()
